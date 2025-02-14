642214d69e1e051a7634e08da9106123f3be7bf3
maxi-jp <juventudperdia@gmail.com>
2025-02-14 13:52:01 +0100

    [Input] hotfix: M and N keyboard keys were crossed.

--- src/input.js
+++ src/input.js
@@ -15,7 +15,7 @@ const KEY_TAB    = 9;
 
 const KEY_Q = 81, KEY_W = 87, KEY_E = 69, KEY_R = 82, KEY_T = 84, KEY_Y = 89, KEY_U = 85;
 const KEY_A = 65, KEY_S = 83, KEY_D = 68, KEY_F = 70, KEY_G = 71, KEY_H = 72, KEY_J = 74;
-const KEY_Z = 90, KEY_X = 88, KEY_C = 67, KEY_V = 86, KEY_B = 66, KEY_M = 78, KEY_N = 77;
+const KEY_Z = 90, KEY_X = 88, KEY_C = 67, KEY_V = 86, KEY_B = 66, KEY_N = 78, KEY_M = 77;
 
 const KEY_0 = 48;
 const KEY_1 = 49;
@@ -75,7 +75,7 @@ var Input = {
 // #region Setup Functions
     SetupKeyboardEvents: function() {
         AddEvent(document, "keydown", function(e) {
-            console.log(e.keyCode);
+            //console.log(e.keyCode);
             // avoid when the key is being held down such that it is automatically repeating
             if (!e.repeat) {
                 Input.keyboard.keydown[e.keyCode] = true;
