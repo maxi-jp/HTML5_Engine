9bda2e8a9e04d0ab296ab6689f6e284a24ad3b3d
maxi-jp <juventudperdia@gmail.com>
2025-04-30 17:00:34 +0200

    [Engine] new class (HTMLMenu) to control HTML + CSS menus

---
+++ src/htmlmenu.js
@@ -0,0 +1,39 @@
+class HTMLMenu {
+    constructor(game, menuContainerSelector, canvasContainerSelector, canvas) {
+        this.game = game;
+
+        this.container = document.querySelector(menuContainerSelector);
+        this.canvasContainer = document.querySelector(canvasContainerSelector);
+
+        this.elements = [];
+        this.buttons = [];
+        this.canvas = canvas;
+    }
+
+    Start() {
+        this.buttons = [];
+
+        // setup the size
+        this.container.setAttribute('style', `width: ${this.canvas.width}px; height: ${this.canvas.height}px;`);
+        this.canvasContainer.setAttribute('style', `width: ${this.canvas.width}px; height: ${this.canvas.height}px;`);
+    }
+
+    SetupElements(elementsSelectors) {
+        elementsSelectors.forEach(selector => {
+            this.elements[selector] = document.querySelector(selector);
+        });
+    }
+
+    SetupButtons(buttonsSelectorsAndCallbacks) {
+        this.buttons = buttonsSelectorsAndCallbacks.map(button =>
+            document.querySelector(button.selector));
+
+        for (let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].addEventListener('click', buttonsSelectorsAndCallbacks[i].callback);
+        }
+    }
+
+    SetContainerStyle(style) {
+        this.container.setAttribute('style', style);
+    }
+}
